286b00d2656a387151cd541299ccbc6845433c18
Bruno Luiz Gonçalves do Nascimento <bruno.gnascimento@linx.com.br>
2023-08-08 22:55:15 -0300

    Operadores de atribuição e ternario

---
+++ 5_op_expressoes/29_op_ternario/index.php
@@ -0,0 +1,19 @@
+<?php
+
+// True
+echo 20 > 10 ? "Deu True <br>" : "Deu False <br>";
+
+
+// False
+echo 5 > 10 ? "Deu True <br>" : "Deu False <br>";
+
+// Ternario com Variaveis
+
+$a = 10;
+$b = 5;
+
+echo $a >= $b ? "DEU TRUE <br>" : "DEU FALSE <br>";
+
+echo $a === $b ? "DEU TRUE <br>" : "DEU FALSE <br>";
+
+echo $a === $b || 10 > 5 ? "DEU TRUE <br>" : "DEU FALSE <br>";
