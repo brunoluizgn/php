dfc9cf2a1b0f7912345061a64b87a89f016349e8
Bruno Luiz Gonçalves do Nascimento <bruno.gnascimento@linx.com.br>
2023-10-18 22:33:38 -0300

    exercicio do while

---
+++ 7_estruturas_de_repeticao/8_do_while/index.php
@@ -0,0 +1,24 @@
+<?php
+
+$j = 0;
+
+do {
+
+    echo "Testando do while $j <br>";
+
+    $j++;
+
+} while($j < 10);
+
+echo "<br>";
+
+$i = 10;
+
+do {
+
+    echo "TESTANDO O DO WHILE 2 $i <br>";
+
+    $i--;
+
+} while ($i > 0);
+
